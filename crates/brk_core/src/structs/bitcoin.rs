d78c39fd8c3071443612080f884cf2dacd031c67
nym21 <expat-epochs0i@icloud.com>
2025-04-19 11:45:26 +0200

    computer + kibo: part 14 - fixes

--- crates/brk_core/src/structs/bitcoin.rs
+++ crates/brk_core/src/structs/bitcoin.rs
@@ -23,27 +23,27 @@ pub struct Bitcoin(f64);
 impl Add for Bitcoin {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        Self(self.0 + rhs.0)
+        Self::from(Sats::from(self) + Sats::from(rhs))
     }
 }
 
 impl Mul for Bitcoin {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
-        Self(self.0 * rhs.0)
+        Self::from(Sats::from(self) * Sats::from(rhs))
     }
 }
 
 impl Div<usize> for Bitcoin {
     type Output = Self;
     fn div(self, rhs: usize) -> Self::Output {
-        Self(self.0 / rhs as f64)
+        Self::from(Sats::from(self) / rhs)
     }
 }
 
 impl From<Sats> for Bitcoin {
     fn from(value: Sats) -> Self {
-        Self(u64::from(value) as f64 / (u64::from(Sats::ONE_BTC) as f64))
+        Self(f64::from(value) / (f64::from(Sats::ONE_BTC)))
     }
 }
 
