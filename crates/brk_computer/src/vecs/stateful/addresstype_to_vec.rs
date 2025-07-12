25e1c25b6d074941ef70cb9669e1846d18762985
nym21 <expat-epochs0i@icloud.com>
2025-07-13 00:21:20 +0200

    computer: perf again

--- crates/brk_computer/src/vecs/stateful/addresstype_to_vec.rs
+++ crates/brk_computer/src/vecs/stateful/addresstype_to_vec.rs
@@ -2,5 +2,20 @@ use derive_deref::{Deref, DerefMut};
 
 use super::ByAddressType;
 
-#[derive(Debug, Default, Deref, DerefMut)]
+#[derive(Debug, Deref, DerefMut)]
 pub struct AddressTypeToVec<T>(ByAddressType<Vec<T>>);
+
+impl<T> Default for AddressTypeToVec<T> {
+    fn default() -> Self {
+        Self(ByAddressType {
+            p2pk65: vec![],
+            p2pk33: vec![],
+            p2pkh: vec![],
+            p2sh: vec![],
+            p2wpkh: vec![],
+            p2wsh: vec![],
+            p2tr: vec![],
+            p2a: vec![],
+        })
+    }
+}
