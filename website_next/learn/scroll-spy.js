c9d5a62fcb2e8b8a0df667069a79a56ad9d612a3
nym21 <expat-epochs0i@icloud.com>
2026-06-09 17:01:28 +0200

    website: redesign part 29

--- website_next/learn/scroll-spy.js
+++ website_next/learn/scroll-spy.js
@@ -122,7 +122,6 @@ export function initScrollSpy(main) {
 
     const hash = `#${section.id}`;
     if (navigatingTo) {
-      selectHash(hash);
       if (hash === navigatingTo) navigatingTo = null;
       return;
     }
